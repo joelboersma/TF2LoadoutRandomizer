6c23e7452affba9e8fb54eb9ab6cb6452a85b018
Joel Boersma <joelboersma@gmail.com>
2021-09-11 16:56:29 -0700

    print each class > slot > weapon

--- generator.py
+++ generator.py
@@ -5,4 +5,10 @@ data = json.load(fr)
 fr.close()
 
 for tf2Class in data.get("classes", {}):
-   print(tf2Class.get("name"))
+   print(tf2Class.get("name", ""))
+   weapons = tf2Class.get("weapons", {})
+   slots = weapons.keys()
+   for slot in slots:
+      print ("    " + slot)
+      for weapon in weapons[slot]:
+         print("        " + weapon)
